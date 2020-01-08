d552313432db6c4eeb1b4ccc9d3ba192aae50033
Madhavi <madhavi.0108@gmail.com>
2020-01-08 00:02:30 -0800

    added a function displayStoredNotes() to display the data from localStorage for each time slot and colorCodeTheTimeSlot() to color code the timeslots  to reflect whether the time slot is in the past, the present, or the future.

--- script.js
+++ script.js
@@ -52,7 +52,79 @@
 
     }
 
+    //function 
+       function colorCodeTheTimeSlot(key, row_id) {
+          var now = moment();
+
+         // var now = moment("9AM", "hA");
+
+          console.log("now is " + now.format("hA"));
+
+          var toBeTestedDate = moment(key, "hA");
+          var diff = now.diff(toBeTestedDate, "hours");
+          if (diff > 0) {
+             //if the difference between now(current hour) and the selected hour is positive, then the selected hour is an old hour to current hour. 
+             //So old hour row should be color coded in grey by using past class
+             console.log(`"${key} is an old hour"`);
+
+             $("#" + row_id).find("textarea").attr({ class: "past", disabled: true });
+
+          }
+          else if (diff < 0) {
+             console.log(`"${key} is an future hour"`);
+             $("#" + row_id).find("textarea").attr("class", "future");
+          }
+          else {
+             console.log(`"${key} is current hour"`);
+             $("#" + row_id).find("textarea").attr("class", "present");
+
+          }
+       }
+
+    function displayStoredNotes()
+    {
+      //  alert("loading data /... ");
+      var data;
+      
+      var key;
+     
+      for(let i=9;i<=17;i++)
+      {
+         var row_id = "row"+i;
+         console.log("row_id "+row_id);
+         
+        // key = i<12 ? `${i}AM` : `${i}PM`;
+         if(i<12)
+         {
+            key = `${i}AM`;
+            console.log("key for "+i+" is"+key);
+            
+         }
+         else if(i===12)
+         {
+            key =  `${i}PM`;
+         }
+         else
+         {
+               key =  `${i-12}PM`
+         }
+        colorCodeTheTimeSlot(key,row_id);
+         data = localStorage.getItem(key);
+         // alert("data at "+key+"is = "+data);
+         $("#"+row_id).find("textarea").text(data);
+
+      }
+
+
+
+    }
+
+    //function displayStoredNotes displays the already stored notes from the local storage on the page refresh.
+    displayStoredNotes();
     $(".saveBtn").on("click",saveTheNote);
+
+
+
 
     });
     
